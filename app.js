4164016aff7bc2fe05b62f84f2dd1fb073b7245e
Biagio Arobba <barobba@gmail.com>
2011-11-11 14:06:33 -0700

    Arranged javascript to make use of HTML elements and stylesheets
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

--- app.js
+++ app.js
@@ -7,35 +7,36 @@ $(document).ready( function () {
     // Get the remote data
     var song_url = "./data/songs.json";
     var song_callback = function(data) {
- 
-      
-      $.each(data.nodes, function(key, value) { 
-      // alert(key+' : '  + value.node.title +'  :  ' + value.node.field_song_audio_value); 
-        var $newdiv1 = $('<h2> '+ value.node.title +'</h2>');
-        $('body').append($newdiv1);
-        var $newdiv2 = $('<h2> Artist: '+ value.node.name +'</h2>');
-        $('body').append($newdiv2);
-        var $player = $('<object id= player" name="player" width="23" height="12">'+
-'<param name="movie" value="http://liveandtell.com/sites/liveandtell.com/modules/audio/player.swf?filename='+value.node.field_song_audio_value+'">'+
-'<param name="wmode" value="transparent"><embed id="player" name="player" type="application/x-shockwave-flash" '+
-'wmode="transparent" src="http://liveandtell.com/sites/liveandtell.com/modules/audio/player.swf?filename='+value.node.field_song_audio_value+     '"width="23" height="12"></object>');
+      $.each(data.nodes, function(index, song_record) { 
+        var song = song_record.node;
+        if (index < 10) {
+          // For testing, we're limiting the number of songs to display
+        
+          // alert(index+' : '  + song.title +'  :  ' + song.field_song_audio_value); 
+          
+          // Song element
+          var songTheme = $('<div class="song" />');
+          songTheme.appendTo('#songs');
 
-$('body').append($player);
-      });
+          // Song player
+          var songPlayer = $('<div class="song-player"><span class="label label-player"></span> <span class="song-player"><object id=player" name="player" width="23" height="12">' + '<param name="movie" value="http://liveandtell.com/sites/liveandtell.com/modules/audio/player.swf?filename=' + song.field_song_audio_value +'">' + '<param name="wmode" value="transparent"><embed id="player" name="player" type="application/x-shockwave-flash" ' + 'wmode="transparent" src="http://liveandtell.com/sites/liveandtell.com/modules/audio/player.swf?filename=' + song.field_song_audio_value + '"width="23" height="12"></object></div>');
+          songPlayer.appendTo(songTheme);
+          
+          // Song title
+          var songTitle = $('<div class="song-title"><span class="label label-song-title">Title: </span> <span class="song-title">'+ song.title +'</span></div>');
+          songTitle.appendTo(songTheme);
+          
+          // Song posted by
+          var songPostedBy = $('<div class="song-posted-by"><span class="label label-artist">Posted by:</span> <span class="song-artist">' + song.name +'</span></div>').appendTo(songTheme);
+          
+          
+        } // end if
+        
+      }); // end $.each
       
+    }; // end song_callback
     
-    };
-    $.ajax(
-      song_url,
-      {
-        dataType: 'json',
-        success: song_callback
-      }
-    );
- 
-  
-  
-     
+    // Make ajax call
+    $.ajax(song_url, {dataType: 'json', success: song_callback});
+    
 });
-
-
